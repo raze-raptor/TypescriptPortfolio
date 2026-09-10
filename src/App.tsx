c28be5d999cd025c6b091d30b26504f5f9f0ded1
Matt <matiyengamatthew@gmail.com>
2026-09-10 09:54:53 +0100

    Added terminal window with 'whoami'

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 function App() {
-  return <div></div>
+  return (
+
+  <div id="hero" className="hero min-h-screen bg-base-200">
+    <div className="mockup-code w-full max-w-2xl">
+      <pre data-prefix="~">
+        <code className="text-lg text-green-500 w-full max-w-2x1">whoami</code>
+      </pre>
+    </div>
+  </div>
+
+  )
 }
 
 export default App
