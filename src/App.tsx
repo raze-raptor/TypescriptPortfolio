22f8343ab3d3654d34ca03138a8375f37cca39b5
Matt <matiyengamatthew@gmail.com>
2026-09-10 21:54:41 +0100

    Added a blinking cursor effect onto

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   <div id="hero" className="hero min-h-screen bg-base-200">
     <div className="mockup-code w-full max-w-2xl">
       <pre data-prefix="~">
-        <code className="text-lg text-green-500 w-full max-w-2x1">whoami</code>
+        <code className="text-lg typewriter-cursor text-green-500 w-full max-w-2x1">whoami</code>
       </pre>
     </div>
   </div>
