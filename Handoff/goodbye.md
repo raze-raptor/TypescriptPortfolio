8a12fcdd9df9db5cf311ef7af60fc5cf98a87dbc
Matt <matiyengamatthew@gmail.com>
2026-09-10 23:58:21 +0100

    Added a better background, glass div and whoami text box alongside a press enter text

---
+++ Handoff/goodbye.md
@@ -0,0 +1,6 @@
+# goodbye.md
+**goodbye.md** is here so that I know what I can log my next steps forwards when i leave my pc after building for a while.
+
+
+# Next Task:
+Fix "whoami" TEXT not cenered
--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,18 @@ function App() {
 
 
       {/* code box */}
-      <div className="mockup-code w-full max-w-2xl">
-        <pre data-prefix="~$">
-          <code className="text-lg typewriter-cursor text-green-500 w-full max-w-2x1">whoami</code>
-        </pre>
+      <div className="glass p-4 rounded-lg shadow-lg w-145 max-w-2xl h-56 flex flex-col justify-center items-center">
+        <div className="mockup-code w-full max-w-2xl">
+          <pre data-prefix=":~$">
+            <code id="whoami" className="text-lg typewriter-cursor text-green-500 w- max-w-2x1 whoami">whoami</code>
+          </pre>
+        </div>
+        {/*end of code box */}
+
+        <p id="start-execute" className="elipsis bounce5s">press enter</p>
       </div>
-      {/*end of code box */}
+      
+      
 
 
     </div>
